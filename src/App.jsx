629a02c6bef0228e53b1b98896e8c82e98c74e0f
Rahul <2019121@iiitdmj.ac.in>
2024-03-21 02:04:05 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import Signup from "./components/Signup";
-import { Routes, Route } from "react-router-dom";
+
 import Homepage from "./components/Homepage";
 function App() {
   return <Signup />;
