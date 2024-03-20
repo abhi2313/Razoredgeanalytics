844a8e3eb918724a266e03a090d555f06e382cca
Rahul <2019121@iiitdmj.ac.in>
2024-03-21 02:05:58 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import "./App.css";
 import Signup from "./components/Signup";
 
-import Homepage from "./components/Homepage";
 function App() {
   return <Signup />;
 }
